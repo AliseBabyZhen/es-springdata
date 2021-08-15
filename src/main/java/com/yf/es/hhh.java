e110640061cf673281ae2a8ea911516a777713e6
cc <hifgh>
2021-08-15 12:26:42 +0800

    SSS

--- src/main/java/com/yf/es/hhh.java
+++ src/main/java/com/yf/es/hhh.java
@@ -12,15 +12,6 @@ public class hhh {
     public static void main(String[] args) {
 
         System.out.println(8888);
-        System.out.println(9999);
-        System.out.println(6666);
-
-        System.out.println(1111);
-
-        System.out.println(6666);
-        System.out.println(6666);
-        System.out.println(0000);
-
     }
 
 }
