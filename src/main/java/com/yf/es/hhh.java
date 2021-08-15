b2b414704664744d37341cbd2a1a9f1a670e9d1f
cc <hifgh>
2021-08-15 13:22:13 +0800

    c

--- src/main/java/com/yf/es/hhh.java
+++ src/main/java/com/yf/es/hhh.java
@@ -11,7 +11,7 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class hhh {
     public static void main(String[] args) {
 
-        System.out.println(88);
+        System.out.println(80);
 
     }
 
